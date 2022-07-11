f061bbc1ab2e886c534731779adb4dd37c6ac984
Nikolay <nikola05@list.ru>
2022-07-11 18:43:17 +0300

    Добавил дз

---
+++ ДЗ/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+
+void PrintNatNub (int a, int b)
+{
+    if (a > b) return ;
+    if ( b % 2 == 1)
+    {b --;
+
+    }
+    PrintNatNub(a, b-2);
+    Console.Write($"{b}; ");
+}
+
+int M = Prompt("Ведите первое число промежутка [M, N} => ");
+int N = Prompt("Ведите первое число промежутка [M, N} => ");
+PrintNatNub(M, N);
---
+++ ДЗ/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8 -> 30
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int M = Prompt("Ведите первое число промежутка [M, N} => ");
+int N = Prompt("Ведите первое число промежутка [M, N} => ");
+
+int PrintNatSum (int a, int b)
+{
+    if (a > b) return 0 ;
+  
+    return PrintNatSum(a, b-1)+b;
+}
+
+int sum =PrintNatSum(M, N);
+Console.WriteLine($"Произведение натуральных чисел равно {sum}");
